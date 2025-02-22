fb678967fc322e50915f274d1c53a69de145baaf
simon <Simon.hinojosa01@Unicatolica.edu.co>
2025-02-21 19:47:44 -0500

    Se le añada Divicion

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -23,8 +23,20 @@ function multiply(a, b){
     return number1 * number2;
 }
 
+function divide(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+
+    if (number2 === 0) {
+        throw new Error("No se puede dividir por cero");
+    }
+
+    return number1 / number2;
+}
+
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    divide
 }
--- calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
+++ calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
@@ -6,5 +6,5 @@ router
     .post('/sumar', calculadoraControllers.sumar)
     .post('/restar', calculadoraControllers.restar)
     .post('/multiplicar', calculadoraControllers.multiplicar)
-
+    .post('/dividir', calculadoraControllers.dividir)
 module.exports = router;

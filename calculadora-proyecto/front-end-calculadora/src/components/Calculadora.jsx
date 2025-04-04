3931e0dd3f0d49cd21ca28337d43ae3909b6d834
Miguel Gómez <bydemon101@gmail.com>
2025-04-03 21:30:16 -0500

    Arreglo

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -6,8 +6,6 @@ function Calculadora(){
     const [number1, setNumber1] = useState('');
     const [number2, setNumber2] = useState('');
     const [resultado, setResultado] = useState('');
-    const [orders, setOrders] = useState([]); // New state for orders
-    const [orderInput, setOrderInput] = useState(''); // New state for order input
 
     function handleSubmit(e){
         e.preventDefault();
@@ -36,16 +34,7 @@ function Calculadora(){
                 <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="dividir" onClick={handleSubmit}/>
             </form>
-            <form onSubmit={handleOrderSubmit}>
-                <input 
-                    type="text" 
-                    value={orderInput} 
-                    onChange={(e) => setOrderInput(e.target.value)} 
-                    placeholder="Captura un pedido"
-                />
-                <input type="submit" value="Agregar Pedido" />
-            </form>
-            <Resultado resultado={"El resultado es "+ resultado} orders={orders} /> {/* Pass orders to Resultado */}
+            <Resultado resultado={"El resultado es "+ resultado}  />
         </div>
     )
 }
